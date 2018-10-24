e46061364d881b68c975477970637000e0c62f28
Fabian Deutsch <fabiand@fedoraproject.org>
2018-10-24 23:40:25 +0200

    test: Fix logs
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-kubectl logs -l kubevirt.io=virt-handler
+kubectl logs -l kubevirt.io=virt-handler --all-containers
 kubectl get events --all-namespaces
 kubectl get pods --all-namespaces
 kubectl describe nodes | tee /dev/stderr | grep devices.kubevirt.io/tun
